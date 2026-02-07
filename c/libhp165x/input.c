8442bd599945b9b115eca0278f8b29cb0014a929
Alexander Pruss <arpruss@gmail.com>
2026-02-07 00:11:19 -0600

    misc

--- c/libhp165x/input.c
+++ c/libhp165x/input.c
@@ -93,7 +93,6 @@ int getTextWithTimeout(char* _buffer, uint16_t maxSize, int timeoutTicks) {
 		while(!kbhit()) {
 			if (timeoutTicks > 0 && endTime <= getVBLCounter()) {
 				clearCursor();
-				printf("%d %d %d\n", timeoutTicks, getVBLCounter(), endTime);
 				return ERROR_TIMEOUT;
 			}
 		}
