de83547b5f199b4611176eadc13a106b0bd5509b
Alexander Pruss <arpruss@gmail.com>
2026-04-18 19:50:42 -0500

    foo

---
+++ screenshot/install.sh
@@ -0,0 +1,2 @@
+sh patch.sh && cp SYSTEM.HFE d:/DSKA0007.HFE
+echo d:/DSKA0007.HFE
---
+++ screenshot/patch.sh
@@ -0,0 +1,3 @@
+make
+python patch.py screendump.S68 ../../floppy/hxc/SYSTEM_ patched
+python ../lifutils.py put SYSTEM.HFE patched SYSTEM_ C001
